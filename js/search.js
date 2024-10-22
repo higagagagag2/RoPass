ac993d6987a8fea166755e167dfe0bcede4aa01e
dtbsisco <dasiscow@gmail.com>
2024-10-22 23:20:10 +0200

    1.1.3

--- js/search.js
+++ js/search.js
@@ -11,18 +11,17 @@ function filterResults() {
     if (query) {
         clearIcon.style.display = 'block';
         const results = [
-            { text: query, label: 'in Experiences', url: `https://www.roblox.com/discover/?Keyword=${query}`, class: 'experience-item' },
-            { text: query, label: 'in People', url: `https://www.roblox.com/search/users?keyword=${query}`, class: 'people-item' },
-            { text: query, label: 'in Groups', url: `https://www.roblox.com/search/groups?keyword=${query}`, class: 'groups-item' },
-            { text: query, label: 'in Marketplace', url: `https://www.roblox.com/catalog?CatalogContext=1&Keyword=${query}`, class: 'marketplace-item' }
+            { text: query, label: 'in Experiences', url: `https://www.roblox.com/discover/?Keyword=${query}`, icon: 'experiences.png', class: 'experience-item' },
+            { text: query, label: 'in People', url: `https://www.roblox.com/search/users?keyword=${query}`, icon: 'people.png', class: 'people-item' },
+            { text: query, label: 'in Groups', url: `https://www.roblox.com/search/groups?keyword=${query}`, icon: 'groups.png', class: 'groups-item' },
+            { text: query, label: 'in Marketplace', url: `https://www.roblox.com/catalog?CatalogContext=1&Keyword=${query}`, icon: 'marketplace.png', class: 'marketplace-item' }
         ];
 
         results.forEach(result => {
             const div = document.createElement('div');
             div.className = `dropdown-item ${result.class}`;
+            div.innerHTML = `<img src="images/${result.icon}" class="dropdown-icon"> <span class="search-text">${result.text}</span> <span>${result.label}</span>`;
 
-            div.innerHTML = `<span class="search-text">${result.text}</span> <span>${result.label}</span>`;
-            
             div.onclick = () => {
                 window.location.href = result.url;
             };
